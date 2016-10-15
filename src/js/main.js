8ba91c15c6434ed349357c74b29059ba1c1a9239
王伟 <greatbuger@gmail.com>
2016-10-15 21:20:18 +0800

    整体完成，但还有一些小细节需要调整

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 /**
  * Created by wei on 2016/10/13.
  */
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            autoplayHoverPause: true
+        }
+
+    );
+});
